67f0c0c787de919537d226fa2467401b60254ed1
Rob Atherton <rob@rase.rocks>
2025-10-16 17:29:24 +0100

    Adds to the implementation of ReadBlock.updateChildren(:) to set the hasChildren property appropriately post update

--- Tests/NotionSwiftTests/Models/ReadBlockTests.swift
+++ Tests/NotionSwiftTests/Models/ReadBlockTests.swift
@@ -30,4 +30,101 @@ struct ReadBlockTests {
         
     }
     
+    @Test func updateChildrenSetsHasChildren() throws {
+        
+        let partialUser = PartialUser(id: .init(UUID().uuidString))
+        let base = ReadBlock(
+            id: .init(UUID().uuidString),
+            archived: .random(),
+            type: .divider,
+            createdTime: .distantPast,
+            lastEditedTime: .now,
+            hasChildren: false,
+            createdBy: partialUser,
+            lastEditedBy: partialUser
+        )
+        
+        let child = ReadBlock(
+            id: .init(UUID().uuidString),
+            archived: .random(),
+            type: .divider,
+            createdTime: .distantPast,
+            lastEditedTime: .now,
+            hasChildren: false,
+            createdBy: partialUser,
+            lastEditedBy: partialUser
+        )
+        
+        try #require(base.hasChildren == false)
+        try #require(base.children.isEmpty)
+        try #require(child.hasChildren == false)
+        try #require(child.children.isEmpty)
+        
+        let result = base.updateChildren([child])
+        
+        #expect(result.hasChildren)
+        #expect(result.children == [child])
+        
+        let childResult = try #require(result.children.first)
+        
+        #expect(childResult.hasChildren == false)
+        #expect(childResult.children.isEmpty)
+        
+    }
+    
+    @Test func descriptionSkipsChildrenCountWithZero() {
+        
+        let partialUser = PartialUser(id: .init(UUID().uuidString))
+        let base = ReadBlock(
+            id: .init(UUID().uuidString),
+            archived: .random(),
+            type: .divider,
+            createdTime: .distantPast,
+            lastEditedTime: .now,
+            hasChildren: false,
+            createdBy: partialUser,
+            lastEditedBy: partialUser
+        )
+        
+        let result = base.description
+        
+        #expect(result.contains("Children") == false)
+        #expect(result.last != ",")
+        
+    }
+    
+    @Test func descriptionContainsChildrenCount() {
+        
+        let partialUser = PartialUser(id: .init(UUID().uuidString))
+        let base = ReadBlock(
+            id: .init(UUID().uuidString),
+            archived: .random(),
+            type: .divider,
+            createdTime: .distantPast,
+            lastEditedTime: .now,
+            hasChildren: false,
+            createdBy: partialUser,
+            lastEditedBy: partialUser
+        )
+        
+        let child = ReadBlock(
+            id: .init(UUID().uuidString),
+            archived: .random(),
+            type: .divider,
+            createdTime: .distantPast,
+            lastEditedTime: .now,
+            hasChildren: false,
+            createdBy: partialUser,
+            lastEditedBy: partialUser
+        )
+        
+        let result = base
+            .updateChildren([child])
+            .description
+        
+        #expect(result.contains("Children:1"))
+        #expect(result.last != ",")
+        
+    }
+    
 }
